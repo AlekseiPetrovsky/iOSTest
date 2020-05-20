425dda10e568901e63452942a2370a0e3f591d39
aleksei.petrovski <aleksei.petrovski>
2020-05-20 16:46:01 -0700

    initial

---
+++ Test/Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Aleksei Petrovski on 5/20/20.
+//  Copyright © 2020 OnX Maps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
